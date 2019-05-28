290bed130286afbb08656e10e6ca2581290b6062
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-05-28 20:14:05 +0200

    more

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use reo_rs::rbpa::StatePred;
+
 use hashbrown::HashMap;
 use reo_rs::proto::traits::Proto;
 
 use reo_rs::set;
 use reo_rs::LocId;
-use std::ops::Range;
+
 // use std::io::Write;
 use itertools::Itertools;
 use num_bigint::BigUint;
+use std::fmt;
+
 
 fn main() {
     let port_set = set! {0, 1};
@@ -27,7 +31,7 @@ fn main() {
         .collect();
     println!("mutex_pairs {:?}", &mutex_pairs);
     let p = powerset(rbpa.rules.len(), mutex_pairs.iter().copied());
-    let mut guards: Vec<_> = p
+    let mut clusters: Vec<_> = p
         .into_iter()
         .map(|set| {
             let mut guard = HashMap::default();
@@ -37,32 +41,112 @@ fn main() {
             (set, guard)
         })
         .collect();
-    for g in guards.iter() {
+    for g in clusters.iter() {
         println!("{:?}", g);
     }
     let mut to_drop = vec![];
-    for g in guards.iter().enumerate().combinations(2) {
+    for g in clusters.iter().enumerate().combinations(2) {
         let (ai, (ak, av)) = g[0];
         let (bi, (bk, bv)) = g[1];
         if av == bv {
             if ak.len() > bk.len() {
                 to_drop.push(bi);
-                println!("({:?}) {:?} encompasses ({:?}) {:?}", bk, bv, ak, av);
+                println!("{:?} encompasses {:?} for guard {:?}", ak, bk, av);
             } else {
                 to_drop.push(ai);
-                println!("({:?}) {:?} encompasses ({:?}) {:?}", ak, av, bk, bv,);
+                println!("{:?} encompasses {:?} for guard {:?}", bk, ak, av);
             }
         }
     }
     to_drop.sort();
     to_drop.dedup();
     for &d in to_drop.iter().rev() {
-        guards.remove(d);
+        clusters.remove(d);
     }
     println!("============");
-    for g in guards.iter() {
-        println!("{:?}", g);
+    let rule_set: Vec<_> = clusters.iter().flat_map(|x| x.0.iter()).copied().unique().collect();
+    // let mutex_set: HashSet<[LocId;2]> = mutex_pairs.iter().copied().collect();
+    for (rules, guard) in clusters.iter() {
+    	println!("{:?}:\tguard: {:?}", rules, PrintableGuard(guard));
+        let mut excluded_guards: Vec<StatePred> = vec![];
+        'outer: for &r1 in &rule_set {
+        	if rules.contains(&r1) {
+        		continue; // rule is INCLUDED!
+        	}
+        	// must exclude!
+        	let r1_guard = rbpa.rules[r1].get_guard();
+	        for e in excluded_guards.iter_mut() {
+	        	if let Some(fused) = fuse_guards(e, r1_guard, true) {
+        			// need to make sure this fusion doesn't capture too much
+	        		if fuse_guards(e, guard, false).is_none() {
+		        		*e = fused;
+		        		continue 'outer;
+	        		} 
+	        	}
+	        }
+	        // failed to fuse. add it.
+	        excluded_guards.push(r1_guard.clone());
+        }
+        for e in excluded_guards.iter() {
+        	println!("exclude: {:?}", PrintableGuard(e));
+        }
     }
+}
+
+struct PrintableGuard<'a>(&'a StatePred);
+impl<'a> fmt::Debug for PrintableGuard<'a> {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		let mut buf = vec!['.';5];
+		for (&k, &v) in self.0.iter() {
+            if buf.len() <= k {
+                buf.resize_with(k + 1, || '.');
+            }
+            buf[k] = if v { 'T' } else { 'F' };
+        }
+        for b in buf.drain(..) {
+            write!(f, "{}", b)?;
+        }
+        Ok(())
+	}
+}
+
+enum FuseCase {
+    LeftSubsumes,
+    RightSubsumes,
+    PartitionAt(LocId),
+    Identical,
+}
+
+fn fuse_guards(l: &StatePred, r: &StatePred, discrepency_ok: bool) -> Option<StatePred> {
+	use FuseCase::*;
+    let mut g_case = Identical;
+    for id in l.keys().chain(r.keys()).unique() {
+        match [l.get(id), r.get(id)] {
+            [Some(_), None] => match g_case {
+                LeftSubsumes | Identical => g_case = LeftSubsumes,
+                _ => return None,
+            }
+            [None, Some(_)] => match g_case {
+                RightSubsumes | Identical => g_case = RightSubsumes,
+                _ => return None,
+            }
+            [Some(a), Some(b)] if a!=b => match g_case {
+                Identical if discrepency_ok => g_case = PartitionAt(*id),
+                _ => return None,
+            }
+            [Some(_), Some(_)] => (),
+            [None, None] => unreachable!(),
+        }
+    }
+    Some(match g_case {
+        Identical | LeftSubsumes => l.clone(),
+        RightSubsumes => r.clone(),
+        PartitionAt(id) => {
+            let mut x = l.clone();
+            let _ = x.remove(&id);
+            x
+        }
+    })
 }
 
 /// Given an end to a range of port ids (presuming port range 0..`rng_end`)
